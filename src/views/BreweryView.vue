a793dd257024b58ecbe1d0c39dc1613099e1a64a
Christopher (Ted) Burns <ceburns111@gmail.com>
2022-08-14 17:57:55 -0400

    feat: added basic brewery components

--- src/views/BreweryView.vue
+++ src/views/BreweryView.vue
@@ -1,29 +1,34 @@
 <script>
-// import { breweryService } from "../services/breweryService";
+import { breweryService } from "../services/breweryService";
 // import { ref } from "vue";
-//import { useRoute } from 'vue-router';
+import { useRoute } from "vue-router";
 export default {
   name: "BreweryView",
   data() {
-    return { items: null };
+    return {
+      brewery: null,
+    };
   },
-  // mounted() {
-  //   const route = useRoute();
-  //   this.brewery = breweries.filter(function(x) { x.id == route.params.id});
-  // },
+  async created() {
+    const route = useRoute();
+    const breweryId = route.params.id;
+    console.log(`brewery id: ${breweryId}`);
+
+    const breweryResult = await breweryService.getBreweryById(breweryId);
+    this.brewery = breweryResult;
+  },
 };
 </script>
 
 <template>
   <div>
-    <h1>NOOOOOO</h1>
+    <PvCard>
+      <template #title>{{ brewery.name }}</template>
+      <template #subtitle>{{ brewery.state + " " + brewery.postal_code}}</template>
+      <template #content>
+        <a v-bind:href="brewery.website_url">Website</a>
+      </template>
+      <template #footer></template>
+    </PvCard>
   </div>
-  <!-- <PvDataTable :value="breweries">
-    <PvColumn field="name" header="Name"></PvColumn>
-    <PvColumn field="street" header="Address"></PvColumn>
-    <PvColumn field="city" header="City"></PvColumn>
-    <PvColumn field="state" header="State"></PvColumn>
-    <PvColumn field="postal_code" header="Zip"></PvColumn>
-    <PvColumn field="website_url" header="Website"></PvColumn>
-  </PvDataTable> -->
 </template>
